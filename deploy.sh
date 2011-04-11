7a8696facd3d29dad9f1830c60ce3145d4050def
Chuck Swanberg <cswanberg@mad-swan.com>
2011-04-11 14:53:59 -0400

    Remove output dir each time we deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
-rm -rf output
+rm -rf ../output
 git checkout source
 nanoc3 co
 rm -rf .sass-cache
+cp -R output ../
 git checkout master
-rm -rf output
-cp -R output/* .
+rm -rf ./*
+cp -R ../output/* .
 git add .
 git commit -a -m "Updated site on `date`"
 git push origin master
